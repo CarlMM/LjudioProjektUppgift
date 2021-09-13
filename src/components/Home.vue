2e94eae0825e8cdb1bde478479e1f0607ea17d8a
CarlMM <kaptenkeps@gmail.com>
2021-09-13 19:36:20 +0200

    Searchbar + button

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
       <h1>HomeView</h1>
+
+
+      <input type="text" placeholder="Search">
+      <button>Sök</button>
   </div>
 </template>
+
+
+<script>
+
+
+export default {
+  
+
+  
+}
+</script>
