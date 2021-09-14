34f8b1e3fa91006efc999380cf7062603616a0ae
CarlMM <kaptenkeps@gmail.com>
2021-09-14 11:11:14 +0200

    Added routing to About.vue+Contact.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '/src/components/Home.vue'
+import About from '/src/components/About.vue'
+import Contact from '/src/components/Contact.vue'
 import MusicDetails from '/src/components/MusicDetails.vue'
 
 
@@ -8,6 +10,16 @@ const routes = [
         path: '/',
         name: 'Home',
         component: Home,
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About,
+    },
+    {
+        path: '/contact',
+        name: 'Contact',
+        component: Contact,
     },
     {
         path: '/musicdetails/:browseId',
