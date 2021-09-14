c3700c5a30f63f78491f02b2323f4939dc60fdb2
CarlMM <kaptenkeps@gmail.com>
2021-09-14 11:05:46 +0200

    Searchresults as routerlink + pathing to correct vue comp

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -4,7 +4,10 @@
         <div id="searchLoop" 
         v-for="result in getYTMusic" 
         :key="result.browseId">
+        <router-link type="button" :to="`/musicdetails/${result.browseId}`">
             <p>{{result.name}}</p>
+        </router-link>
+        
             
         </div>
     </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '/src/components/Home.vue'
+import MusicDetails from '/src/components/MusicDetails.vue'
+
+
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: Home,
+    },
+    {
+        path: '/musicdetails/:browseId',
+        name: 'MusicDetails',
+        component: MusicDetails,
     },
 ]
 const router = createRouter({
