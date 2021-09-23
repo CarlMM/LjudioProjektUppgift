7007f2942e65b8887b65d4f8636a7d02b8a759af
CarlMM <kaptenkeps@gmail.com>
2021-09-23 11:11:15 +0200

    Mediaqueries fixing

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,69 @@
 <template>
-    <div>
-        <h1>About page</h1>
-    </div>
+  <div>
+      <h1>About page</h1>
+      <div id="aboutDiv">
+        <div>
+            <p>
+              1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic,
+              necessitatibus earum. Impedit soluta possimus dicta quia nobis ratione
+              illum est adipisci minima earum! Magni dignissimos accusamus fugiat ea
+              quis recusandae!
+            </p>
+        </div>
+        <div>
+            <p>
+              2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic,
+              necessitatibus earum. Impedit soluta possimus dicta quia nobis ratione
+              illum est adipisci minima earum! Magni dignissimos accusamus fugiat ea
+              quis recusandae!
+            </p>
+        </div>
+        <div>
+            <p>
+              3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic,
+              necessitatibus earum. Impedit soluta possimus dicta quia nobis ratione
+              illum est adipisci minima earum! Magni dignissimos accusamus fugiat ea
+              quis recusandae!
+            </p>
+        </div>
+        <div>
+            <p>
+              4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic,
+              necessitatibus earum. Impedit soluta possimus dicta quia nobis ratione
+              illum est adipisci minima earum! Magni dignissimos accusamus fugiat ea
+              quis recusandae!
+            </p>
+        </div>
+      </div>
+  </div>
 </template>
+
+
+<style scoped>
+h1{
+    text-align: center;
+}
+#aboutDiv  {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr;
+}
+
+#aboutDiv > div{
+    padding: 15px
+}
+
+#aboutDiv > p {
+  text-align: center;
+  
+}
+
+@media screen and (max-width: 400px){
+    #aboutDiv > div > p{
+        font-size: 16px;
+    }
+}
+</style>
+
+
+
